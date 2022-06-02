f67f94bcaae29972f94ea6d292427d4d83c5f868
jeongjimin <wlals3968@naver.com>
2022-06-02 17:21:48 +0900

    Add:Django Project Setting

---
+++ my_settings.py
@@ -0,0 +1,14 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '34th',
+        'USER': 'root',
+        'PASSWORD': '19980105',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-*f%v0oo98vnvumq_zju84**0os4oobug03_qgluuyjzq)o*2pr'
+
+
